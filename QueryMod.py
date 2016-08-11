91e5845452d83ccb37e916faf3f0c5f44c08b41f
Teagan-Wilson <teagan.wilson@shicksolutions.com>
2016-08-11 14:37:14 -0500

    Update QueryMod.py

--- QueryMod.py
+++ QueryMod.py
@@ -1,8 +1,9 @@
 #Graylog Stream Query Service
 #Teagan Wilson
 #7/29/16
-#Pulls Data from a Graylog Stream, parses it and inserts into a local MongoDB instance
+#Pulls Data from a Graylog Stream, parses it and inserts into a MongoDB instance
 from pymongo import MongoClient
+import pymongo
 import json
 import requests
 import time
@@ -17,33 +18,36 @@ db = client.queue
 timestamp = int(time.time())
 ipurl = 'https://graylog.shickusa.com:12900/search/universal/relative?query=*&range=60&limit=1000&filter=streams:5798f1ad4894b436df435fa6'
 dnsurl = 'https://graylog.shickusa.com:12900/search/universal/relative?query=*&range=60&limit=1000&filter=streams:579a55dc4894b42940bbad7f'
-auth = ("Display", "S++HMefn9W6CRPLyZBH^mrGb@WK8RAqbcKQ-f*aEPt5CBh3zSb^SrUkE8wjT-45vdhKpfdWj*5LaM&AQ&!&ExtnsAQu6RP3U!hP$5e+&^@CeTJVxuX#Gp-NLM^2G23WX") #not the real password or user for that matter :P
+auth = ("", "")
 params = {'fields': 'IP,Name', 'Accept' : 'application/json'}
 headers = {'Accept': 'application/json','fields': 'IP'}
 
 
 #Query Stream Function
-def QueryGLS (url):
+def QueryGLS(url):
 	response = requests.get(url, auth=auth, params=params, headers=headers)
 	loaded_content = json.loads(response.content)
 	return loaded_content
 
-#Insert DNS Function
-def insertDNS( ip, timestamp ):
+#Insert IP Function
+def insertIP( ip, timestamp ):
 	result = db.tolookupip.insert_one({ "ip": ip, "timestamp": timestamp})
 
-#Insert IP Function
-def insertDNS( url, timestamp ):
+#Insert DNS Function
+def insertDNS( url , timestamp ):
 	result = db.tolookupdnso.insert_one({ "url": url, "timestamp": timestamp})
+	print result.inserted_id
 	
 #Get Streams from Graylog
-slipstreamIP = QueryGLS(ipurl)
+#slipstreamIP = QueryGLS(ipurl)
+
+
 slipstreamDNS = QueryGLS(dnsurl)
 
 #Process IP Stream and Insert into Queue
-for ip in slipstreamIP['messages']:
-	print(ip['message']['IP'])
-	insert(ip['message']['IP'],timestamp)
+#for ip in slipstreamIP['messages']:
+#	print(ip['message']['IP'])
+#	insert(ip['message']['IP'],timestamp)
  
 #Process DNS logs and Insert into Queue
 for ip in slipstreamDNS['messages']:
@@ -53,6 +57,5 @@ for ip in slipstreamDNS['messages']:
 	ip['message']['Name'] = pat.sub("",ip['message']['Name'])
 	pat = re.compile("(\(\w+\))+")
 	ip['message']['Name'] = pat.sub(".",ip['message']['Name'])
-	print(ip['message']['Name'])
-	insert(ip['message']['Name'],timestamp)
+	insertDNS(ip['message']['Name'],timestamp)
 	
